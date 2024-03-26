ae342533ff2d1c72ecf1ffc12213b22a76821535
Shakeel Ahmed <shakeel_kashmiri@outlook.com>
2024-03-26 12:57:40 +0500

    CommitedCode.

---
+++ index.ts
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+
+import inquirer from 'inquirer';
+
+const answer=await inquirer.prompt([
+    {
+        name:'fahrenheit',
+        type:'number',
+        message:'Enter the fahrenheit value'
+    }
+])
+
+console.log(answer.fahrenheit);
+console.log("Convert to celcius")
+console.log(answer.fahrenheit-32*5/9);
